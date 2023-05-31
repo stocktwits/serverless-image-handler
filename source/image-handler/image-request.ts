4879d9eb72d302025a562bc026b1301324bf325f
Siyanatullah Khan <siyanatullah.khan@stocktwits.com>
2023-05-31 16:05:44 +0530

    [GROWTH-1032] limited gif resize

--- source/image-handler/image-request.ts
+++ source/image-handler/image-request.ts
@@ -124,6 +124,7 @@ export class ImageRequest {
       imageRequestInfo.edits = this.parseImageEdits(event, imageRequestInfo.requestType);
 
       const originalImage = await this.getOriginalImage(imageRequestInfo.bucket, imageRequestInfo.key);
+  
       imageRequestInfo = { ...imageRequestInfo, ...originalImage };
 
       imageRequestInfo.headers = this.parseImageHeaders(event, imageRequestInfo.requestType);
