fe84952003bb1db171ce8a1ffa6160bbd1799fe8
Siyanatullah Khan <siyanatullah.khan@stocktwits.com>
2023-07-19 19:37:11 +0530

    morefix

--- source/image-handler/image-request.ts
+++ source/image-handler/image-request.ts
@@ -83,7 +83,10 @@ export class ImageRequest {
    * @param imageRequestInfo Initialized image request information
    */
   private fixQuality(imageRequestInfo: ImageRequestInfo): void {
-    console.log("fix Quality Image Infor"+ JSON.stringify(imageRequestInfo));
+    let imageRequestInfoCopy = { ...imageRequestInfo };
+    delete imageRequestInfoCopy.originalImage;
+    console.log("fix Quality Image Infor"+ JSON.stringify(imageRequestInfoCopy));
+
     if (imageRequestInfo.outputFormat) {
       const requestType = [RequestTypes.CUSTOM, RequestTypes.THUMBOR];
       const acceptedValues = [
