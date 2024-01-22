961677922a17ed7644134a6f2da54f9d7a01abdd
Siyanatullah Khan <siyanatullah.khan@stocktwits.com>
2024-01-22 18:44:49 +0530

    Added more ehaders

--- source/image-handler/image-request.ts
+++ source/image-handler/image-request.ts
@@ -257,15 +257,30 @@ public async getImageBytesUsingPuppeteer(imageUrl) {
 
       const page = await browser.newPage();
       await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
-      await page.setExtraHTTPHeaders({
-        'referer': 'www.google.com',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'accept-language': 'en-US,en;q=0.9',
-        'cookie': 'prov=4568ad3a-2c02-1686-b062-b26204fd5a6a; usr=p=%5b10%7c15%5d%5b160%7c%3bNewest%3b%5d',
-      });
+      const headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Sec-Ch-Ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Sec-Ch-Ua-Platform': '"macOS"',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+        'Upgrade-Insecure-Requests': '1'
+      };
+
+     await page.setExtraHTTPHeaders(headers);
       
       // Navigate directly to the image URL
       const response = await page.goto(imageUrl, { waitUntil: 'networkidle2' });
+      
+      if(response.status() !== 200) {
+        await browser.close();
+        throw new Error(`Failed to get the image at ${imageUrl}. Status code: ${response.status()}` );
+      }
+
       const buffer = await response.buffer();
 
       await browser.close();
