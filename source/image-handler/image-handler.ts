2d7ee59408293603763f455c80fa4e901b084437
Siyanatullah Khan <siyanatullah.khan@stocktwits.com>
2024-02-29 09:09:05 +0530

    [GROWTH-2422] reverse orientation based on value

--- source/image-handler/image-handler.ts
+++ source/image-handler/image-handler.ts
@@ -40,7 +40,8 @@ export class ImageHandler {
       // Check if orientation is present and log it
       if (metadata.orientation) {
         console.log("Orientation: " + metadata.orientation);
-        image = sharp(originalImage, options).withMetadata({ orientation: metadata.orientation });
+        this.setReverseRotation(edits, metadata.orientation);
+        image = sharp(originalImage, options).withMetadata({ orientation: metadata.orientation }); //This meta data is only for output purpose 
       } else {
         console.log("No orientation metadata found.");
         image = sharp(originalImage, options).withMetadata();
@@ -49,6 +50,40 @@ export class ImageHandler {
 
     return image;
   }
+
+  private setReverseRotation(edits: ImageEdits, orientation: number): void {
+    switch (orientation) {
+        case 1:
+            edits.rotate = 0; // No rotation needed
+            break;
+        case 2:
+            // For horizontal flip, we don't adjust rotation; this case is handled here for completeness
+            edits.rotate = 0;
+            break;
+        case 3:
+            edits.rotate = 180; // Reverse of 180 is 180
+            break;
+        case 4:
+            // For vertical flip, we don't adjust rotation; this case is handled here for completeness
+            edits.rotate = 0;
+            break;
+        case 5:
+            edits.rotate = 90; // Reverse of transposed (270 rotated then flipped) is 90
+            break;
+        case 6:
+            edits.rotate = 270; // Reverse of 90 degrees clockwise is 270 degrees
+            break;
+        case 7:
+            edits.rotate = 90; // Reverse of transverse (90 rotated then flipped) is 270, but considering rotation only
+            break;
+        case 8:
+            edits.rotate = 90; // Reverse of 270 degrees clockwise is 90 degrees
+            break;
+        default:
+            edits.rotate = 0; // Default case, no rotation
+    }
+}
+
  
   
 
